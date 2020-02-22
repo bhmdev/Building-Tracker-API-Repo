2afe951e03905728d30433a344d3809d4ceda719
bhmdev <bmontima@gmail.com>
2020-02-22 16:38:27 -0500

    Added Macros to Models AND Serializers

--- app/serializers/building_serializer.rb
+++ app/serializers/building_serializer.rb
@@ -1,3 +1,4 @@
 class BuildingSerializer < ActiveModel::Serializer
   attributes :id, :address, :price, :built_date, :sq_feet
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_205308) do
+ActiveRecord::Schema.define(version: 2020_02_22_205644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_02_22_205308) do
     t.integer "sq_feet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_buildings_on_user_id"
   end
 
   create_table "examples", force: :cascade do |t|
@@ -49,5 +51,6 @@ ActiveRecord::Schema.define(version: 2020_02_22_205308) do
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 
+  add_foreign_key "buildings", "users"
   add_foreign_key "examples", "users"
 end
